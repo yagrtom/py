467c85dfe15f70df1faa5365bd2021dedc0408f9
yagrtom <160411694+yagrtom@users.noreply.github.com>
2024-05-13 16:50:00 +0300

    Add files via upload

--- DZ/ex4/ex4.py
+++ DZ/ex4/ex4.py
@@ -1,38 +1,9 @@
 import json
-import numpy as np
-from scipy.special import spherical_jn, spherical_yn, spherical_in
+from scipy.special import jn, yn, hankel2
+import matplotlib.pyplot as plt
 
 
-#Класс для записи (формирования) ответа
-class RadarCrossSection:
-    def __init__(self, frequency_range, radius):
-        self.frequency_range = frequency_range
-        self.radius = radius
-#Фунция подсчета
-    def calculate_rcs(self):
-        rcs_data = []
-        for freq in self.frequency_range:
-            wavelength = 3e8 / freq
-            k = 2 * np.pi / wavelength
-            a_n = spherical_jn(0, k * self.radius) / spherical_in(0, k * self.radius)
-            b_n = (k * self.radius * spherical_jn(0, k * self.radius) - spherical_jn(1, k * self.radius)) / \
-                  (k * self.radius * spherical_yn(0, k * self.radius) - spherical_yn(1, k * self.radius))
-            rcs = (wavelength*wavelength / np.pi) * np.abs((np.sum([(-1)**n * (n + 0.5) * (b_n - a_n) for n in range(1, 10)]))**2)
-            rcs_data.append({"freq": freq, "lambda": wavelength, "rcs": rcs})
-
-        return rcs_data
-#класс для вывода результата в файл
-class ResultsOutput:
-    def __init__(self, data):
-        self.data = data
-
-    def save_to_json(self, filename):
-        with open(filename, "w") as file:
-            json.dump({"data": self.data}, file, indent=4)
-
-
-
-#чтение с файла
+#Чтение из файла
 import urllib.request
 import xml.etree.ElementTree as ET
 url = 'https://jenyay.net/uploads/Student/Modelling/task_rcs.xml'
@@ -44,15 +15,55 @@ for elem in tree.iter('variant'):
         D = float(elem.attrib['D'])
         fmin = float(elem.attrib['fmin'].replace('e9', '')) * 1e9
         fmax = float(elem.attrib['fmax'].replace('e9', '')) * 1e9
-#print (D, fmin, fmax)
+r = D/2
+
+class RCS:
+    def __init__(self, freq_range, r):
+        self.freq_range = freq_range
+        self.r = r
+
+    def calculate_rcs(self):
+        lambda_range = [1/fmax, 1/fmin]
+        data = []
+        for freq in self.freq_range:
+            k = 2 * 3.14159 * freq
+            rcs = 0
+            for n in range(1, 20):
+                a_n = jn(n, k*self.r) / hankel2(n, k*self.r)
+                b_n = (k*self.r*jn(n-1, k*self.r) - n*jn(n, k*self.r)) / (k*self.r*hankel2(n-1, k*self.r) - n*hankel2(n, k*self.r))
+                rcs += (-1)**n * (n + 0.5) * (b_n - a_n)
+            rcs = (lambda_range[0]**2 / 3.14159) * abs(rcs)**2
+            data.append({"freq": freq, "lambda": lambda_range[0], "rcs": rcs})
+        return data
+
+class Output:
+    def __init__(self, data):
+        self.data = data
+
+    def save_to_json(self, filename):
+        with open(filename, 'w') as f:
+            json.dump({"data": self.data}, f, indent=4)
+
+    def plot_data(self):
+        freq = [d["freq"] for d in self.data]
+        rcs = [d["rcs"] for d in self.data]
+        plt.plot(freq, rcs)
+        plt.xlabel('Frequency')
+        plt.ylabel('RCS')
+        plt.title('RCS vs Frequency')
+        plt.grid()
+        plt.show()
+
+freq_range = range(1, 11)
 
 
 
-if __name__ == "__main__":
-    frequency_range = np.linspace(fmin, fmax, 100)  # Frequency range from 0.1 GHz to 10 GHz
-    radius = D/2
-    rcs_calculator = RadarCrossSection(frequency_range, radius)
-    rcs_data = rcs_calculator.calculate_rcs()
+r = D/2
 
-    output = ResultsOutput(rcs_data)
-    output.save_to_json("results.json")
+
+rcs_calculator = RCS(freq_range, r)
+data = rcs_calculator.calculate_rcs()
+
+output = Output(data)
+output.save_to_json('result.json')
+output.plot_data()
